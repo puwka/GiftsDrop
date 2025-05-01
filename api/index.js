bd980e4de2705529129d4629c33e954249393ab9
Максим Кануркин <epicgames4438@gmail.com>
2025-05-01 13:54:35 +0300

    фикс 17

--- api/index.js
+++ api/index.js
@@ -2,11 +2,13 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
+const path = require('path');
 
 const app = express();
 
 app.use(cors());
 app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, '../public')));
 
 // Подключаем роуты
 const usersRoutes = require('./users');
