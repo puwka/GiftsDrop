0d662007e008924352f6495c55d6e81839b9cdf8
Максим Кануркин <epicgames4438@gmail.com>
2025-05-01 11:41:48 +0300

    фикс откат

--- gift-drop-backend/index.js
+++ gift-drop-backend/index.js
@@ -23,7 +23,7 @@ app.get('/', (req, res) => {
 });
 
 // Подключаем маршруты
-const usersRoutes = require('../gift-drop-backend/routes/users');
+const usersRoutes = require('../routes/users');
 
 app.use('/api/users', usersRoutes);
 
